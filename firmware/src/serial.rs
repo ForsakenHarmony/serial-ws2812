ac299cb975a733c135a15488ebc11145d090624c
hrmny <8845940+ForsakenHarmony@users.noreply.github.com>
2024-10-01 01:51:08 +0200

    update toolchain and deps

--- firmware/src/serial.rs
+++ firmware/src/serial.rs
@@ -63,7 +63,6 @@ pub async fn usb_serial_task(driver: Driver<'static, USB>, id: [u8; ID_BYTES]) {
 
 	// Create embassy-usb DeviceBuilder using the driver and config.
 	// It needs some buffers for building the descriptors.
-	let mut device_descriptor = [0; 256];
 	let mut config_descriptor = [0; 256];
 	let mut bos_descriptor = [0; 256];
 	let mut control_buf = [0; 128];
@@ -73,7 +72,6 @@ pub async fn usb_serial_task(driver: Driver<'static, USB>, id: [u8; ID_BYTES]) {
 	let mut builder = Builder::new(
 		driver,
 		config,
-		&mut device_descriptor,
 		&mut config_descriptor,
 		&mut bos_descriptor,
 		&mut [], // no msos descriptors
